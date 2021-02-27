d28833ac9f217d734867c8b36cb161a710fea1dd
Jklein64 <2022jklein@tjhsst.edu>
2021-02-26 20:20:10 -0500

    Hide nav button on big screens; comment media query code

--- frontend/src/js/navigation.js
+++ frontend/src/js/navigation.js
@@ -18,7 +18,20 @@ const nav_bar = MDCTopAppBar.attachTo(document.querySelector(".nav-bar"))
 nav_bar.root.querySelector(".nav-bar__menu-button").addEventListener("click", () => (drawer.open = !drawer.open))
 
 // make nav-drawer a modal on small devices and standard on large ones
-const mq = window.matchMedia("only screen and (min-width: 1200px)")
-const on_mq_change = ({ matches }) => drawer.root.classList[matches ? "remove" : "add"]("mdc-drawer--modal")
+const mq = window.matchMedia("only screen and (min-width: 1200px)") // this media query isn't related to anything else
+const on_mq_change = ({ matches }) => {
+    // Switches the drawer from modal to always visible once the breakpoint is reached.
+    drawer.root.classList[matches ? "remove" : "add"]("mdc-drawer--modal")
+
+    // Shifts the nav_bar to the left to accomodate for the space taken up once the drawer is always visible.
+    nav_bar.root.classList[matches ? "add" : "remove"]("nav-bar--shifted-left")
+
+    // Hide the nav-bar's button to open the drawer when the drawer is always visible.
+    const nav_button = nav_bar.root.querySelector(".nav-bar__menu-button")
+    nav_button.classList[matches ? "add" : "remove"]("nav-bar__menu-button--hidden")
+
+    const root = document.querySelector(".root")
+    root.style.paddingLeft = matches ? `${drawer.root.getBoundingClientRect().width}px` : null
+}
 mq.addEventListener("change", on_mq_change)
 on_mq_change({ matches: mq.matches })
