37c86a4cbaa215a8247fac5836587af81d9befa6
Jklein64 <2022jklein@tjhsst.edu>
2021-03-12 15:23:32 -0500

    Add skeleton for officer button interativity.

--- frontend/src/js/pages/about.js
+++ frontend/src/js/pages/about.js
@@ -1,1 +1,5 @@
+import { add_ripple } from "../button"
 import "../navigation"
+
+const officer_buttons = Array.from(document.querySelectorAll(".officer__social")).map(add_ripple)
+officer_buttons.forEach(button => (button.unbounded = true))
