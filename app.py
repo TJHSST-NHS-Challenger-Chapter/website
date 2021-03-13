5f6124f3c1df1776cfe942b4864865aa49a0a2d2
Jklein64 <2022jklein@tjhsst.edu>
2021-03-12 19:34:32 -0500

    Deadline calendar download in progress.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # TODO Create API endpoints for facebook and google classroom announcements.
 
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(
     __name__,
@@ -10,12 +11,21 @@ app = Flask(
 )
 
 fake_deadlines = [{
+    # UUID, or Universally Unique IDentifier
+    "id": "b4f513ad-ef15-4a86-920d-b844f435a90c",
     "title": "Assignment Name",
-    "due_date": "12/1/2020",
+    # Unix Epoch, or seconds since Jan 1 1970
+    "due_date": "1615594637",
     "description": "Description or additional information about the assignment.  Don’t put too much in here though (link to things like google classroom instead?)"
 }, {
+    "id": "4bc7cb92-1cba-4eb8-a400-2cbf5eaa8162",
     "title": "Assignment Name 2",
-    "due_date": "12/5/2020",
+    "due_date": "1615594658",
+    "description": "Description or additional information about the assignment.  Don’t put too much in here though (link to things like google classroom instead?)"
+}, {
+    "id": "44db5396-fb12-4d09-b88a-49702d3e37fb",
+    "title": "Assignment Name 2",
+    "due_date": "1615594674",
     "description": "Description or additional information about the assignment.  Don’t put too much in here though (link to things like google classroom instead?)"
 }]
 
@@ -36,13 +46,33 @@ fake_announcements = [{
     "link": "https://www.google.com"
 }]
 
+
 @app.route("/")
 def index():
     # TODO: fetch classroom and facebook announcements on each HTTP request
-    return render_template("home.html", deadlines=fake_deadlines, announcements=fake_announcements)
+
+    # turn Unix Epoch into mm/dd/yyyy before sending back
+    deadlines_with_readable_date = list()
+    for deadline in fake_deadlines:
+        copy = deadline.copy()
+        copy["due_date"] = datetime.fromtimestamp(
+            int(copy["due_date"])).strftime("%-m/%-d/%Y")
+        deadlines_with_readable_date.append(copy)
+    return render_template("home.html",
+                           deadlines=deadlines_with_readable_date,
+                           announcements=fake_announcements)
 
 
-@app.route("/about")
+@app.route("/api/v1/deadlines/<id>")
+def get_deadline_by_id(id):
+    """ Returns an iCal calendar event that matches the deadline with the specified id. """
+    # TODO: The google sheets api allows us to specify the format of the date when we're reading it.  https://developers.google.com/sheets/api/guides/formats.  We should use this.
+    for deadline in fake_deadlines:
+        if deadline["id"] == id:
+            return deadline
+
+
+@app.route("/about/")
 def about():
     return render_template("about.html")
 
--- frontend/src/js/pages/home.js
+++ frontend/src/js/pages/home.js
@@ -2,7 +2,17 @@ import { add_ripple } from "../button"
 import "../navigation"
 
 const calendar_buttons = Array.from(document.querySelectorAll(".deadline__button")).map(add_ripple)
-calendar_buttons.map(b => b.root.addEventListener("click", () => console.log("[download the individual event now]")))
+calendar_buttons.map(b =>
+    b.root.addEventListener("click", e => {
+        const deadline = e.target.parentElement.parentElement.parentElement
+        fetch(`${window.location.origin}/api/v1/deadlines/${deadline.id}`)
+            .then(raw => raw.json())
+            .then(json => {
+                // TODO: turn the JSON into a .ics file
+                console.log(json)
+            })
+    })
+)
 
 const calendar_all_button = add_ripple(document.querySelector(".deadlines__button"))
 calendar_all_button.root.addEventListener("click", () => console.log("[download all of the events now]"))
