867aaa891a3647c85f7395a6809fca0a7846c7f0
Jklein64 <2022jklein@tjhsst.edu>
2021-03-17 18:24:51 -0400

    Add routes to flask app

--- app.py
+++ app.py
@@ -77,5 +77,20 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/service")
+def service():
+    return render_template("service.html")
+
+
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
