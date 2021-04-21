0920be2448f4f31a327c9977767642b1d266734b
Jklein64 <2022jklein@tjhsst.edu>
2021-04-21 19:15:17 -0400

    Read data from google sheet for home page
    The data is reread each request, so the home page unfortunately
    takes around 2-3 seconds to load properly.  Additionally, the data
    is hosted on a spreadsheet under my personal google account
    (kleinjason234@gmail.com) until I have access to put it on the non-
    FCPS NHS one.  The app depends on the capitalization and spelling
    of the headers.

--- app.py
+++ app.py
@@ -2,7 +2,17 @@
 # TODO Create API endpoints for facebook and google classroom announcements.
 
 from flask import Flask, render_template, request
-from datetime import datetime
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+credential = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"])
+client = gspread.authorize(credential)
+SPREADSHEETS = client.open("Website database")
 
 app = Flask(
     __name__,
@@ -10,66 +20,25 @@ app = Flask(
     static_folder="frontend/build"
 )
 
-fake_deadlines = [{
-    # UUID, or Universally Unique IDentifier
-    "id": "b4f513ad-ef15-4a86-920d-b844f435a90c",
-    "title": "Assignment Name",
-    # Unix Epoch, or seconds since Jan 1 1970
-    "due_date": "1615594637",
-    "description": "Description or additional information about the assignment.  Don’t put too much in here though (link to things like google classroom instead?)"
-}, {
-    "id": "4bc7cb92-1cba-4eb8-a400-2cbf5eaa8162",
-    "title": "Assignment Name 2",
-    "due_date": "1615594658",
-    "description": "Description or additional information about the assignment.  Don’t put too much in here though (link to things like google classroom instead?)"
-}, {
-    "id": "44db5396-fb12-4d09-b88a-49702d3e37fb",
-    "title": "Assignment Name 2",
-    "due_date": "1615594674",
-    "description": "Description or additional information about the assignment.  Don’t put too much in here though (link to things like google classroom instead?)"
-}]
-
-fake_announcements = [{
-    "title": "Things are Due!",
-    "date": "May 3, 2020",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-    "link": None
-}, {
-    "title": "No More x2VOL!",
-    "date": "May 3, 2020",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-    "link": None
-}, {
-    "title": "Use Google Sheets",
-    "date": "May 3, 2020",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-    "link": "https://www.google.com"
-}]
-
 
 @app.route("/")
 def index():
-    # TODO: fetch classroom and facebook announcements on each HTTP request
-
-    # turn Unix Epoch into mm/dd/yyyy before sending back
-    deadlines_with_readable_date = list()
-    for deadline in fake_deadlines:
-        copy = deadline.copy()
-        copy["due_date"] = datetime.fromtimestamp(
-            int(copy["due_date"])).strftime("%-m/%-d/%Y")
-        deadlines_with_readable_date.append(copy)
+    # TODO: make the fetching faster.  Currently spends 2-3 seconds before page load
+    deadlines, announcements, contact = SPREADSHEETS.worksheets()
+    print("announcements", announcements.get_all_records())
+    print("deadlines", deadlines.get_all_records())
     return render_template("home.html",
-                           deadlines=deadlines_with_readable_date,
-                           announcements=fake_announcements)
+                           deadlines=deadlines.get_all_records(),
+                           announcements=announcements.get_all_records())
 
 
-@app.route("/api/v1/deadlines/<id>")
-def get_deadline_by_id(id):
-    """ Returns an iCal calendar event that matches the deadline with the specified id. """
-    # TODO: The google sheets api allows us to specify the format of the date when we're reading it.  https://developers.google.com/sheets/api/guides/formats.  We should use this.
-    for deadline in fake_deadlines:
-        if deadline["id"] == id:
-            return deadline
+# @app.route("/api/v1/deadlines/<id>")
+# def get_deadline_by_id(id):
+#     """ Returns an iCal calendar event that matches the deadline with the specified id. """
+#     # TODO: The google sheets api allows us to specify the format of the date when we're reading it.  https://developers.google.com/sheets/api/guides/formats.  We should use this.
+#     for deadline in fake_deadlines:
+#         if deadline["id"] == id:
+#             return deadline
 
 
 @app.route("/about")
@@ -101,4 +70,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
