ae8dbb1a2f6d392172a89da9b28c63eab4bc9fbc
Jason Klein <jklein.64@verizon.net>
2021-11-22 17:47:30 -0500

    Make button and form elements interactive

--- frontend/src/js/pages/induction.js
+++ frontend/src/js/pages/induction.js
@@ -1,2 +1,10 @@
+import { MDCTextField } from "@material/textfield"
+
+import { add_ripple } from "../button"
 import "../navigation"
 import "../register"
+
+const seating_chart_button = add_ripple(document.querySelector(".info__button"))
+
+const firstname_text_field = new MDCTextField(document.querySelector(".induction-form__firstname"))
+const lastname_text_field = new MDCTextField(document.querySelector(".induction-form__lastname"))
