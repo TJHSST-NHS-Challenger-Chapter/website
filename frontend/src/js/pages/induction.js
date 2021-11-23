2a273f450db50cfea6efc768315d686c84d11106
Jason Klein <jklein.64@verizon.net>
2021-11-22 19:13:01 -0500

    Add dialog component to display seating info

--- frontend/src/js/pages/induction.js
+++ frontend/src/js/pages/induction.js
@@ -1,4 +1,5 @@
 import { MDCTextField } from "@material/textfield"
+import { MDCDialog } from "@material/dialog";
 
 import { add_ripple } from "../button"
 import "../navigation"
@@ -10,12 +11,12 @@ const firstname_text_field = new MDCTextField(document.querySelector(".induction
 const lastname_text_field = new MDCTextField(document.querySelector(".induction-form__lastname"))
 
 const form = document.querySelector(".induction-form")
+const dialog = new MDCDialog(document.querySelector(".seating-dialog"))
 
 form.addEventListener("submit", e => {
     e.preventDefault()
     const action = form.getAttribute("action")
     const form_data = Object.fromEntries(new FormData(form).entries())
-    console.log(form_data)
     // make AJAX request instead
     fetch(action, {
         method: "POST",
@@ -28,11 +29,18 @@ form.addEventListener("submit", e => {
         .then(json => {
             /* expected shape is {
                 sucess: boolean,
-                region: string,
-                number: number
+                region: string?,
+                number: number?
             } */
             if (json.success) {
-
+                const {firstname, lastname} = form_data
+                const {region, number} = json
+                if (region && number)
+                    dialog.root.querySelector(".seating-dialog__content").innerHTML = `Welcome, <b style="font-weight:bold">${firstname} ${lastname}</b>. Your seat is <b style="font-weight:bold">#${number}</b> at <b style="font-weight:bold">${region}</b>. See the seating chart .pdf file if this doesn't make sense.`
+                else
+                    dialog.root.querySelector(".seating-dialog__content").innerHTML = `I'm sorry, but the name <b style="font-weight:bold">${firstname} ${lastname}</b> doesn't seem to be part of our seating chart database. Please contact one of the NHS officers if you think it should be.`
+                dialog.layout()
+                dialog.open()
                 // TODO show the number and region somehow
                 console.log(json)
             }
